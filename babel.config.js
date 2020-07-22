9bc9caf9b359c2e0b54b30de883ca62b5522cf38
RoyalSix <jay.a.scottjr@biola.edu>
2020-07-22 10:45:48 -0700

    checking out

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	presets: [
-		'@babel/env',
-		'@babel/react',
-	],
-	plugins: ['@babel/transform-runtime']
+  presets: [
+    '@babel/env',
+    '@babel/react',
+  ],
+  plugins: ['@babel/transform-runtime', 'istanbul'],
 };
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,4 @@
 module.exports = (on, config) => {
-  on('file:preprocessor', require('@cypress/code-coverage/use-browserify-istanbul'))
-  on('task', require('@cypress/code-coverage/task'))
+  require('@cypress/code-coverage/task')(on, config);
   return config;
-}
+};
