a56d597d3e9333771d6cf787d510cfb775563a00
ancientTexts-net <zach.pennington@unfoldingWord.org>
2021-02-23 09:01:08 -0600

    WIP

--- src/core/TargetFile.context.js
+++ src/core/TargetFile.context.js
@@ -18,13 +18,19 @@ function TargetFileContextProvider({
 
   const { state: sourceFile } = useContext(FileContext);
 
+  const getTargetFilePath = (_filepath) => {
+    //return "en_tn_57-TIT.tsv";
+    //debugger;
+    return 'tn_57-TIT.tsv';
+  };
+
   const {
     state, actions, component, components, config,
   } = useFile({
     config: (authentication && authentication.config),
     authentication,
     repository: targetRepository,
-    filepath,
+    filepath: getTargetFilePath(filepath),
     onFilepath: setFilepath,
     defaultContent: (sourceFile && sourceFile.content),
   });
@@ -35,56 +41,56 @@ function TargetFileContextProvider({
     );
   }
 
-  useEffect(() => {
-    if (state === undefined || state.content === undefined) {
-      onValidated(false);
-      //onCriticalErrors(['Validating...']);
-    } else if (!validated) {
-      if ( state.name.endsWith('.tsv') ) {
-        const link = state.html_url.replace('/src/', '/blame/');
-        let criticalNotices = [];
-        let tsvFile = state.content.trimEnd();
-        // Split into an array of rows
-        let rows = tsvFile.split('\n');
-        // Is the first row correct (must have the correct headers)
-        let tsvHeader = "Book\tChapter\tVerse\tID\tSupportReference\tOrigQuote\tOccurrence\tGLQuote\tOccurrenceNote";
-        if (tsvHeader !== rows[0]) {
-          criticalNotices.push([
-            `${link}#L1`,
-            '1',
-            `Bad TSV Header, expecting "${tsvHeader.replaceAll('\t', ', ')}"`]);
-        }
+  // useEffect(() => {
+  //   if (state === undefined || state.content === undefined) {
+  //     onValidated(false);
+  //     //onCriticalErrors(['Validating...']);
+  //   } else if (!validated) {
+  //     if ( state.name.endsWith('.tsv') ) {
+  //       const link = state.html_url.replace('/src/', '/blame/');
+  //       let criticalNotices = [];
+  //       let tsvFile = state.content.trimEnd();
+  //       // Split into an array of rows
+  //       let rows = tsvFile.split('\n');
+  //       // Is the first row correct (must have the correct headers)
+  //       let tsvHeader = "Book\tChapter\tVerse\tID\tSupportReference\tOrigQuote\tOccurrence\tGLQuote\tOccurrenceNote";
+  //       if (tsvHeader !== rows[0]) {
+  //         criticalNotices.push([
+  //           `${link}#L1`,
+  //           '1',
+  //           `Bad TSV Header, expecting "${tsvHeader.replaceAll('\t', ', ')}"`]);
+  //       }
   
-        if (rows.length > 1) {
-          for (let i = 1; i < rows.length; i++) {
-            let line = i + 1;
-            let cols = rows[i].split('\t');
-            if (cols.length < 9) {
-              criticalNotices.push([
-                `${link}#L${line}`,
-                `${line}`,
-                `Not enough columns, expecting 9, found ${cols.length}`
-              ])
-            } else if (cols.length > 9) {
-              criticalNotices.push([
-                `${link}#L${line}`,
-                `${line}`,
-                `Too many columns, expecting 9, found ${cols.length}`
-              ])
-            }
-          }
-        }
+  //       if (rows.length > 1) {
+  //         for (let i = 1; i < rows.length; i++) {
+  //           let line = i + 1;
+  //           let cols = rows[i].split('\t');
+  //           if (cols.length < 9) {
+  //             criticalNotices.push([
+  //               `${link}#L${line}`,
+  //               `${line}`,
+  //               `Not enough columns, expecting 9, found ${cols.length}`
+  //             ])
+  //           } else if (cols.length > 9) {
+  //             criticalNotices.push([
+  //               `${link}#L${line}`,
+  //               `${line}`,
+  //               `Too many columns, expecting 9, found ${cols.length}`
+  //             ])
+  //           }
+  //         }
+  //       }
   
-        if (criticalNotices.length > 0) {
-          onCriticalErrors(criticalNotices);
-        } else {
-          onValidated(true);
-        }
-      } else {
-        onValidated(true)
-      }
-    }
-  }, [validated, onValidated, state, onCriticalErrors]);
+  //       if (criticalNotices.length > 0) {
+  //         onCriticalErrors(criticalNotices);
+  //       } else {
+  //         onValidated(true);
+  //       }
+  //     } else {
+  //       onValidated(true)
+  //     }
+  //   }
+  // }, [validated, onValidated, state, onCriticalErrors]);
 
   const context = {
     state: { ...state, validated }, // state true/false
